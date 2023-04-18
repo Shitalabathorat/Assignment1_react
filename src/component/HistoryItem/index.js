055fb0f063823822eb41764da1960552efd6b699
Shitalabathorat <126850348+Shitalabathorat@users.noreply.github.com>
2023-04-18 13:17:53 +0530

    Update index.js

--- src/component/HistoryItem/index.js
+++ src/component/HistoryItem/index.js
@@ -34,3 +34,38 @@ const HistoryItem = props => {
   )
 }
 export default HistoryItem
+
+
+
+import React from 'react'
+import './index.css'
+
+const HistoryItem = props => {
+  const {HistoryItem, deleteItem} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = HistoryItem
+
+  const handelDelete = () => {
+    deleteItem(id)
+  }
+
+  return (
+    <li className="list-item">
+      <div>
+        <p className="">{timeAccessed}</p>
+        <img src={logoUrl} alt="domain logo" />
+        <p>{title}</p>
+        <p>{domainUrl}</p>
+      </div>
+      <div>
+        <button testid="delete" onClick={handelDelete}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default HistoryItem
